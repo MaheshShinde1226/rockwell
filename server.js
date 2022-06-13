cffd5456488e192513515b8206df7544f14aa344
MaheshShinde2612 <shindemahesh1226@gmail.com>
2022-06-13 12:50:48 +0530

    files added

---
+++ server.js
@@ -0,0 +1,38 @@
+/*var count=34;
+count++;
+console.log("Server is running");
+console.log(count);
+
+
+//CTL + SHIFT + C   
+
+*/
+
+var express =require("express");  //lib-----Node Module
+var app=express();  // will create app object from express package
+
+var onAboutUs=function(req, res){
+    res.send("Chief Mentor :Tushar Sthul");
+};
+var onDefault=function(req, res){
+    res.send("<h1>Rockwell Automation Inc.</h1>"+
+              "<hr/>"+
+              "<ol>"+
+                    " <li>Manager:Mahesh Shinde</li>"+
+                    " <li>MD: Naman Saxena</li>"+
+                    " <li>CEO: Amey Joshi</li>"+
+                    " <li>CTO: Prajakta Mutyalwar</li>"+
+             "</ol>");
+};
+app.get("/",onDefault);  // Request handler functions are registered
+app.get("/aboutus",onAboutUs);  // Request handler functions are registered
+var server=app.listen(8081);
+console.log("Server is running on port 8081");
+// if external packages are required
+// we have to download those packages from internet using  package manager tool
+// Runtime--------tools-------------packages-----------------------config file
+// Java-----------maven--------------.jar files----------------------pom.xml
+// Android--------graddle------------android jar file----------------pom.xml
+// Python---------pip----------------Python libraries----------------
+// .NET ---------Nugget--------------dll files-----------------------package.config
+// NodeJS----------npm --------------download node modules-----------package.json 
